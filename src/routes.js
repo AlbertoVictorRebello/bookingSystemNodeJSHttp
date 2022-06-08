594f46180f288fadd286778e36a7e983b0458acd
albertovictorrebello <albertorebello@gmail.com>
2022-06-08 06:22:01 -0300

    feat: add create user feature using mock user from routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-/* const  express = require('express');
-const routes = new express.Router(); */
-
 import { Router } from 'express';
+import User from './app/models/User';
+import Database from './database/index';
+
 const routes = new Router();
 
 routes.get('/api-test', async (req, res) => {
@@ -9,5 +9,16 @@ routes.get('/api-test', async (req, res) => {
         message: 'The API is alive!'
     });
 });
+
+routes.post('/users', async(req, res) => {   
+        const user = await User.create({
+            name: "user-name-1",
+            email: "user1@server.com",
+            password_hash: "123456"
+        });
+        return res.json(user);
+});
+
+
 
 export default routes;
